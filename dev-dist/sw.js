b1a19171704505c342c09e717ea07ae5f98b8969
VanTuDev <Vantu.dev@gmail.com>
2025-11-08 11:14:01 +0700

    update

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -85,7 +85,7 @@ define(['./workbox-13fdf7ef'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.n122b1u1cp"
+    "revision": "0.hbb9eabhsdo"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
