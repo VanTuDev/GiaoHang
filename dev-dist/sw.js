716993a4befc408b6ba699660b263ad35bb98cf2
thanhtungf123 <thanhtungphankt@gmail.com>
2025-11-18 22:47:54 +0700

    update chat

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -85,7 +85,7 @@ define(['./workbox-13fdf7ef'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.ssperek4jeg"
+    "revision": "0.nmuluu4b2hg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
